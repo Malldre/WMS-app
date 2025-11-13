7bebc582dbe42c9b50b7e0a7bd958141fdc27da2
Luiz Felipe <lfleite80@gmail.com>
2025-11-13 18:03:41 -0300

    feat: created visual for tab my tasks

--- src/features/tasks/tabs/myTasks.tsx
+++ src/features/tasks/tabs/myTasks.tsx
@@ -1,9 +1,21 @@
-import { Box, Text } from '@gluestack-ui/themed';
+import CardTasks from '@/src/components/cards';
+import FilterByStatusAndName from '@/src/components/filters/filterByStatusAndName';
+import { ActionsheetFlatList, Box } from '@gluestack-ui/themed';
+
+const data = [
+  { id: 1, status: 'Conferência',  nota: '1231', descricao: 'É uma bota com caixa de equipamentoa aisudbnasiudhnsaiu', especi: 'Bota CAT-23435'},
+  { id: 2, status: 'Estoque', nota: '13434', descricao: 'Equipamenteo de matraosidjasiuodhasuidhasiu', especi: 'Bota CAT-23435'}
+]
 
 export default function MyTasks() {
   return (
-    <Box gap="$2">
-      <Text fontWeight="$bold">Suas tarefas</Text>
+    <Box gap="$4" py='$2'>
+      <FilterByStatusAndName />
+      <ActionsheetFlatList
+        data={data}
+        renderItem={({ item }) => <CardTasks item={item} />}
+        keyExtractor={(item: any) => item.id}
+      />
     </Box>
   );
 }
